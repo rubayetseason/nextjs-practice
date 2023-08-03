280db0213100b89c3f2491a4c2ee337c83a7980f
rubayet-islam <season.ss2001@gmail.com>
2023-08-03 22:21:08 +0600

    nextAuth

--- nextAuth/src/components/Layout/Navbar.js
+++ nextAuth/src/components/Layout/Navbar.js
@@ -6,6 +6,7 @@ import { useSession, signOut } from "next-auth/react";
 const Navbar = () => {
   const { data: session } = useSession();
 
+  console.log("from header", session);
   return (
     <Header
       style={{
